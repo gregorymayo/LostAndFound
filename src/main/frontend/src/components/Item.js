aabb246b04c426548c962085d77c89ff25fd0bf4
Kevin Prakasa <kevinprakasa15@gmail.com>
2020-04-25 03:02:09 -0700

    added page to post items

--- src/main/frontend/src/components/Item.js
+++ src/main/frontend/src/components/Item.js
@@ -1,8 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Navbar from './Navbar';
+import './Item.css';
+
 const Item = () => {
+	const [ itemName, setItemName ] = useState('');
+	const [ itemDesc, setItemDesc ] = useState('');
+	const [ itemImg, setItemImg ] = useState('https://securecdn.pymnts.com/wp-content/uploads/2019/09/Tile-Google-Assistant-Integration.jpg');
+
+	
+	const onSubmit = (e) => {
+    	e.preventDefault();
+    	console.log(itemName);
+		console.log(itemDesc);
+		console.log(itemImg);
+	}
+
+	const handleChange = (e) => {
+		e.preventDefault();
+		if(e.target.id === "itemName")
+			setItemName(e.target.value);
+		if(e.target.id === "itemDesc")
+			setItemDesc(e.target.value);
+		if(e.target.id === "imageUpload")
+			setItemImg(URL.createObjectURL(e.target.files[0]));
+		// if(e.target.id === "itemName")
+		// 	setItemName(e.target.value);
+	};
+	
 	return (
 		<div>
-            <p>Item</p>
+            <Navbar/>
+			<div className="item-container">
+				<div className="item-img-container">
+					<img src={itemImg} className="rounded float-left img-lost" alt="items.jpg"/>
+				</div>
+				<div className="item-form">
+					<form>
+					<div className="form-group">
+						<label>Item Name</label>
+						<input 
+							type="text" 
+							className="form-control" 
+							id="itemName" 
+							placeholder="Enter item name"
+							onChange={event => handleChange(event)}
+							value={itemName}
+							/>
+					</div>
+					<div className="form-group">
+						<label>Item Description</label>
+						<textarea 
+							className="form-control" 
+							id="itemDesc" 
+							rows="3" 
+							placeholder="Describe the color, size, etc."
+							onChange={event => handleChange(event)}
+							value={itemDesc}
+							/>
+					</div>
+					<div className="form-group">
+						<label>Upload Image</label>
+						<input 
+							type="file" 
+							className="form-control-file" 
+							id="imageUpload"
+							onChange={event => handleChange(event)}
+							/>
+							<p>Only upload .JPEG, .JPG, .PNG type file</p>
+					</div>
+					<button className="item-btn-submit" onClick={onSubmit}>Post Item</button>
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 };
