b63cf6f7803a0b6f0285e9dbc041dc360afcbe88
gregorymayo <gregoryjuevesii23@gmail.com>
2020-04-28 02:58:33 -0700

    fix all the issue including the url and the style

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,32 +1,30 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home';
 import User from './components/User';
 import Admin from './components/Admin';
 import Item from './components/Item';
-import Profile from './components/Profile';
 import Error from './components/Error';
-
-import Navigation from './components/Navigation';
+import SignUpForm from './components/SignUpForm';
 
 function App() {
-  return (
-    <div className="App">
-        <BrowserRouter>
-        <div>
-            <Switch>
-                <Route path='/' component={Home} exact />
-                <Route path='/user' component={User}  />
-                <Route path='/admin' component={Admin}  />
-                <Route path='/item' component={Item}  />
-                <Route path='/profile' component={Profile}  />
-                <Route component={Error} />
-            </Switch>
-        </div>
-        </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<div>
+					<Switch>
+						<Route path='/' component={Home} exact />
+						<Route path='/sign-up' component={SignUpForm} />
+						<Route path='/user' component={User} />
+						<Route path='/admin' component={Admin} />
+						<Route path='/item' component={Item} />
+						<Route component={Error} />
+					</Switch>
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
